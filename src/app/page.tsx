2c2751beead458b184808fc337dbdc0a852eafe4
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-09 13:09:13 +0530

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div className="bg-blue-600">
+      <Button>Heelo</Button>
+    </div>
+  );
+};
+
+export default HomePage;
