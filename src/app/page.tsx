41919f94df4b78ebca2c5667e0775e0c573616c1
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-06-09 16:58:07 +0530

    neo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 const HomePage = () => {
   return (
-    <div className="bg-blue-600">
-      <Button>Heelo</Button>
+    <div className="flex flex-col gap-y-4 p-4">
+      <Button variant="elevated">Hello</Button>
+      <Input placeholder="input" />
+      <Progress value={50} />
+      <Textarea placeholder="textarea" />
+      <Checkbox />
     </div>
   );
 };
